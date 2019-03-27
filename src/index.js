7229e1e1f93bbd8837a4e491a2d20157d0ba2cd5
Karolis Stulgys <k.stulgys@gmail.com>
2019-03-27 13:55:32 +1100

    add program modal

--- src/index.js
+++ src/index.js
@@ -3,15 +3,29 @@ import "shards-ui/dist/css/shards.min.css"
 import "./styles.css"
 import React, { useEffect, useState } from "react"
 import ReactDOM from "react-dom"
-import { Button } from "shards-react"
+import { Button, Modal, ModalBody } from "shards-react"
 import OneRMModal from "./OneRMModal"
 import LiftContext from "./LiftContext"
 import AddLift from "./AddLift"
 import store from "./store"
+import image from "./programImage.jpg"
 
 function App() {
   const { state, setState } = store.useStore()
+  const [taps, incTap] = useState(1)
+
   // console.log(state.modifiers)
+
+  const handleTapped = () => {
+    incTap(taps + 1)
+  }
+
+  const showProgram = () => {
+    if (taps % 8 === 0) {
+      return true
+    }
+    return false
+  }
   return (
     <div
       className=" m-0 p-0 row d-flex justify-content-center w-100"
@@ -20,8 +34,8 @@ function App() {
         {state.currentLifts.map((lift, i) => {
           return <LiftContext lift={lift} key={i} />
         })}
-        <br />
-        <br />
+        <div className="py-4 bg-dark" onClick={handleTapped} />
+        {showProgram() && <ProgramModal />}
         <AddLift />
         <div className="my-3">
           <OneRMModal />
@@ -46,6 +60,27 @@ function App() {
           </Button>
         </div>
       </div>
+    </div>
+  )
+}
+
+function ProgramModal() {
+  const [open, toggle] = useState(false)
+
+  return (
+    <div className="w-100">
+      <Button
+        size="lg"
+        theme="info"
+        className="w-100"
+        onClick={() => toggle(!open)}>
+        Program
+      </Button>
+      <Modal className="" open={open} toggle={() => toggle(!open)}>
+        <ModalBody className="p-0">
+          <img src={image} style={{ width: "100%" }} />
+        </ModalBody>
+      </Modal>
     </div>
   )
 }
